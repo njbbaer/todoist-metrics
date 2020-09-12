04186e5c955073ea1e1ebd5d1ac9b2786c1eabd2
Nate Baer <njbbaer@gmail.com>
2020-09-12 02:09:17 -0700

    Change metrics & test webhooks

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from todoist_metrics import TodoistMetrics
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,5 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pressure_score = TodoistMetrics().calculate_pressure_score()
-    return render_template('home.html', pressure_score=pressure_score)
+    todoist = TodoistMetrics()
+    return render_template('home.html', 
+        first_priority  = todoist.count_items(priority=0),
+        second_priority = todoist.count_items(priority=1),
+        third_priority  = todoist.count_items(priority=2)
+    )
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print(request.json)
+    return Response(status=200)
--- todoist_metrics.py
+++ todoist_metrics.py
@@ -5,17 +5,12 @@ from dotenv import load_dotenv
 import os
 
 class TodoistMetrics:
-  PRESSURE_SCORE_TABLE = { 1: 0, 2: 1, 3: 4, 4: 16 }
-
   def __init__(self):
     self.api = todoist.TodoistAPI(os.getenv('TODOIST_API_KEY'))
     self.api.sync()
 
-  def calculate_pressure_score(self):
-    pressure_score = 0
-    for item in self._active_items():
-      pressure_score += self.PRESSURE_SCORE_TABLE[item['priority']]
-    return pressure_score
+  def count_items(self, priority):
+    return sum(item['priority'] == priority for item in self._active_items())
   
   def _items(self):
     return self.api.state['items']
